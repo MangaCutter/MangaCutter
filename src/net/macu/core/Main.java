b8a579a582bc4416062c04c757a97d9a26f8ac84
MasterLogick <31408232+MasterLogick@users.noreply.github.com>
2021-03-06 18:07:57 +0400

    change webp-imageio native lib loading method

--- src/net/macu/core/Main.java
+++ src/net/macu/core/Main.java
@@ -13,9 +13,9 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.reflections.Reflections;
 
 import javax.swing.*;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.io.*;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
 import java.security.Security;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -52,7 +52,7 @@ public class Main {
             }
         }));
 
-        System.loadLibrary("webp-imageio");
+        loadNativeFromJar();
 
         reflections = new Reflections("net.macu");
 
@@ -115,5 +115,82 @@ public class Main {
 
     public static String getVersion() {
         return "v5.1.1";
+    }
+
+    //from com.luciad.imageio.webp.NativeLibraryUrls
+    public static void loadNativeFromJar() {
+        String os = System.getProperty("os.name").toLowerCase();
+        String bits = System.getProperty("os.arch").contains("64") ? "64" : "32";
+        String libFilename = "libwebp-imageio.so";
+        String platform = "linux";
+        if (os.contains("win")) {
+            platform = "win";
+            libFilename = "webp-imageio.dll";
+        } else if (os.contains("mac")) {
+            platform = "mac";
+            libFilename = "libwebp-imageio.dylib";
+        }
+
+        try {
+            InputStream in = Main.class.getResourceAsStream(String.format("/native/%s/%s/%s", platform, bits, libFilename));
+            Throwable var5 = null;
+
+            try {
+                if (in == null) {
+                    throw new RuntimeException(String.format("Could not find WebP native library for %s %s in the jar", platform, bits));
+                }
+
+                File tmpLibraryFile = Files.createTempFile("", libFilename).toFile();
+                tmpLibraryFile.deleteOnExit();
+                FileOutputStream out = new FileOutputStream(tmpLibraryFile);
+                Throwable var8 = null;
+
+                try {
+                    byte[] buffer = new byte[8192];
+
+                    int bytesRead;
+                    while ((bytesRead = in.read(buffer)) != -1) {
+                        out.write(buffer, 0, bytesRead);
+                    }
+                } catch (Throwable var34) {
+                    var8 = var34;
+                    throw var34;
+                } finally {
+                    if (out != null) {
+                        if (var8 != null) {
+                            try {
+                                out.close();
+                            } catch (Throwable var33) {
+                                var8.addSuppressed(var33);
+                            }
+                        } else {
+                            out.close();
+                        }
+                    }
+
+                }
+
+                System.load(tmpLibraryFile.getAbsolutePath());
+            } catch (Throwable var36) {
+                var5 = var36;
+                throw var36;
+            } finally {
+                if (in != null) {
+                    if (var5 != null) {
+                        try {
+                            in.close();
+                        } catch (Throwable var32) {
+                            var5.addSuppressed(var32);
+                        }
+                    } else {
+                        in.close();
+                    }
+                }
+
+            }
+
+        } catch (IOException var38) {
+            throw new RuntimeException("Could not load native WebP library", var38);
+        }
     }
 }
