2ac236df927f4d8b3efee82c138c31191c308314
MasterLogick <31408232+MasterLogick@users.noreply.github.com>
2021-05-25 00:07:11 +0300

    replace reflection with static list

--- src/main/java/net/macu/core/Main.java
+++ src/main/java/net/macu/core/Main.java
@@ -1,31 +1,24 @@
 package net.macu.core;
 
-import net.macu.UI.Form;
-import net.macu.UI.IconManager;
-import net.macu.UI.MainView;
-import net.macu.UI.ViewManager;
+import net.macu.UI.*;
 import net.macu.browser.plugin.BrowserPlugin;
 import net.macu.browser.proxy.cert.CertificateAuthority;
-import net.macu.service.Service;
+import net.macu.service.*;
 import net.macu.settings.L;
 import net.macu.settings.Settings;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.reflections.Reflections;
 
 import javax.swing.*;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.lang.reflect.InvocationTargetException;
 import java.security.Security;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 
 public class Main {
     private static List<Form> forms;
     private static List<Service> services;
-    private static Reflections reflections;
 
     static {
         System.setProperty("org.apache.commons.logging.Log",
@@ -49,31 +42,18 @@ public class Main {
             e.printStackTrace(pw);
             JOptionPane.showMessageDialog(null, "Error: " + sw);
         });
-        reflections = new Reflections("net.macu");
         Settings.loadSettings();
         L.loadLanguageData();
         IconManager.loadIcons();
         ViewManager.setLookAndFeel();
         Security.addProvider(new BouncyCastleProvider());
         CertificateAuthority.loadRootCA();
-        Set<Class<? extends Form>> formsSet = reflections.getSubTypesOf(Form.class);
-        forms = Collections.unmodifiableList(getInstances(formsSet));
-        Set<Class<? extends Service>> servicesSet = reflections.getSubTypesOf(Service.class);
-        services = Collections.unmodifiableList(getInstances(servicesSet));
+        forms = Collections.unmodifiableList(Arrays.asList(new AsIsPageForm(), new ManualForm(), new SinglePageForm(),
+                new ConstantHeightForm(), new MultiPageForm()));
+        services = Collections.unmodifiableList(Arrays.asList(new AcQq(), new Daum(), new LocalFiles(), new ManhuaGui(),
+                new Naver(), new Rawdevart()));
         ViewManager.initFileChoosers();
         IOManager.initClient();
-    }
-
-    private static <T> List<T> getInstances(Set<Class<? extends T>> classSet) {
-        ArrayList<T> instancesList = new ArrayList<>();
-        classSet.forEach(aClass -> {
-            try {
-                instancesList.add(aClass.getConstructor().newInstance());
-            } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-                e.printStackTrace();
-            }
-        });
-        return instancesList;
     }
 
     public static List<Form> getForms() {
@@ -82,10 +62,6 @@ public class Main {
 
     public static List<Service> getServices() {
         return services;
-    }
-
-    public static Reflections getReflections() {
-        return reflections;
     }
 
     public static String getVersion() {
